35cf577a3e1554e50dab3cd4939e48539e7332a0
Witold Swierzy <witold.swierzy@oracle.com>
2026-03-23 13:05:40 +0100

    Initial commit

---
+++ src/main/java/com/oracle/Applier.java
@@ -0,0 +1,110 @@
+package com.oracle;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import oracle.jdbc.pool.OracleDataSource;
+
+public class Applier extends Thread {
+
+    private Connection connection;
+    private OracleDataSource ods;
+
+    private int    threadId;
+    private String tableName;
+    private String tnsAdmin;
+    private String dbName;
+    private String username;
+    private String password;
+    private int    numOfOperations;
+
+    private long startTime    = 0;
+    private long endTime      = 0;
+    private long insStartTime = 0;
+    private long insEndTime   = 0;
+    private long updStartTime = 0;
+    private long updEndTime   = 0;
+    private long delStartTime = 0;
+    private long delEndTime   = 0;
+    private PreparedStatement insert,update,delete;
+
+    public Applier (int threadId,
+                    String tableName,
+                    String tnsAdmin,
+                    String dbName,
+                    String username,
+                    String password,
+                    int numOfOperations) {
+        this.threadId        = threadId;
+        this.tableName       = tableName;
+        this.tnsAdmin        = tnsAdmin;
+        this.dbName          = dbName;
+        this.username        = username;
+        this.password        = password;
+        this.numOfOperations = numOfOperations;
+
+        try {
+            ods = new OracleDataSource();
+            ods.setURL("jdbc:oracle:thin:@" + dbName + "?TNS_ADMIN=" + tnsAdmin);
+            ods.setUser(username);
+            ods.setPassword(password);
+            connection = ods.getConnection();
+            connection.setAutoCommit(true);
+            insert = connection.prepareStatement("INSERT INTO "+this.tableName+" values (?,?)");
+            update = connection.prepareStatement("UPDATE "+this.tableName+" SET SK=? WHERE PK=?");
+            delete = connection.prepareStatement("DELETE FROM "+this.tableName+" WHERE PK=?");
+        }
+        catch (Exception e) {e.printStackTrace();};
+    }
+
+    public void run () {
+        int start = numOfOperations*threadId + 1;
+        int end   = numOfOperations*threadId + numOfOperations + 1;
+        try {
+            // inserts
+            startTime = System.currentTimeMillis();
+            insStartTime = System.currentTimeMillis();
+            for (int i = start; i < end; i++) {
+                insert.setInt(1, i);
+                insert.setString(2, "OldValue");
+                insert.execute();
+            }
+
+            insEndTime = System.currentTimeMillis();
+            // updates
+            updStartTime = System.currentTimeMillis();
+            for (int i = start; i < end; i++) {
+                update.setString(1,"NewValue");
+                update.setInt(2,i);
+                update.execute();
+            }
+            updEndTime = System.currentTimeMillis();
+            // deletes
+            for (int i = start; i < end; i++) {
+                delete.setInt(1,i);
+                delete.execute();
+            }
+            delStartTime = System.currentTimeMillis();
+            delEndTime = System.currentTimeMillis();
+            endTime = System.currentTimeMillis();
+            connection.close();
+        }
+        catch (Exception e) {e.printStackTrace();};
+    }
+
+    public long getExecutionTime() {
+        return this.endTime - this.startTime;
+    }
+
+    public long getInsertTime() {
+        return this.insEndTime - this.insStartTime;
+    }
+
+    public long getUpdateTime() {
+        return this.updEndTime - this.updStartTime;
+    }
+
+    public long getDeleteTime() {
+        return this.delEndTime - this.delStartTime;
+    }
+}
---
+++ src/main/java/com/oracle/Main.java
@@ -0,0 +1,71 @@
+package com.oracle;
+
+import oracle.jdbc.pool.OracleDataSource;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+
+    static String tnsAdmin    = System.getenv("BENCH_TNS_ADMIN");
+    static String dbName      = System.getenv("BENCH_DB");
+    static String username    = System.getenv("BENCH_DB_USER");
+    static String password    = System.getenv("BENCH_DB_PASSWORD");
+    static String tableName   = System.getenv("BENCH_TABLE").toUpperCase();
+    static int threadPoolSize  = Integer.parseInt(System.getenv("BENCH_THREADS"));
+    static int numOfOperations = Integer.parseInt(System.getenv("BENCH_OPS"));
+    static int numOfTables     = 0;
+
+    public static void main() {
+        long startTime=0,endTime=0;
+        Applier[] appliers = new Applier[threadPoolSize];
+        try {
+            System.out.println("Initializing");
+            OracleDataSource ods = new OracleDataSource();
+            ods.setURL("jdbc:oracle:thin:@" + dbName + "?TNS_ADMIN=" + tnsAdmin);
+            ods.setUser(username);
+            ods.setPassword(password);
+            Connection con = ods.getConnection();
+
+            Statement tableExists = con.createStatement();
+            ResultSet resultSet = tableExists.executeQuery("SELECT COUNT(*) NUM_TABLES FROM USER_TABLES WHERE TABLE_NAME = '"+tableName+"'");
+            while (resultSet.next() ) {
+                numOfTables = resultSet.getInt("NUM_TABLES");
+                if (numOfTables != 0)
+                    con.createStatement().execute("DROP TABLE " + tableName);
+            }
+            con.createStatement().execute("CREATE TABLE "+tableName+"(PK NUMBER(10),SK VARCHAR2(1000))");
+            con.close();
+
+            for (int i = 0; i < appliers.length; i++)
+                appliers[i] = new Applier(i,tableName,tnsAdmin,dbName,username,password,numOfOperations);
+
+            System.out.println("Benchmark Test initialized successfully.");
+            System.out.println("Starting Threads");
+            startTime = System.currentTimeMillis();
+            for (int i = 0; i < appliers.length; i++)
+                appliers[i].start();
+
+            for (int i = 0; i < appliers.length; i++)
+                appliers[i].join();
+
+            endTime = System.currentTimeMillis();
+            System.out.println("Test completed.");
+            System.out.println("Table Name                              : "+tableName);
+            System.out.println("Number of threads                       : "+threadPoolSize);
+            System.out.println("Number of INSERT/UPDATE/DELETE ops sets : "+numOfOperations);
+            System.out.println("Total execution time (ms)               : "+(endTime-startTime));
+            System.out.println("Execution times of threads (ms)         : ");
+            for (int i=0; i<threadPoolSize;i++)
+                System.out.println("Thread #"+i+", total : "+appliers[i].getExecutionTime() +
+                        " ,Insert : "+appliers[i].getInsertTime() +
+                        " ,Update : "+appliers[i].getUpdateTime() +
+                        " ,Delete : "+appliers[i].getDeleteTime() );
+        }
+        catch (Exception e) {e.printStackTrace();}
+    }
+}
